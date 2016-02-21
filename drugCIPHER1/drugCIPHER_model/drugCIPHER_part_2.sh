f929918b9549933f044818bc22a68d1756356298
XinWANG822 <myaddress@gmail.com>
2016-02-21 14:40:59 +0800

    Deleted some files

--- drugCIPHER1/drugCIPHER_model/drugCIPHER_part_2.sh
+++
@@ -1,6 +0,0 @@
-# Watch out which format this file is! Is it UNIX?! 
-matlab_exec=/usr/local/matlab/bin/matlab
-function="drugCIPHER_part_2('../view_in/chemical_similarity.txt')"
-echo ${function} > drugCIPHER_part_2_exec.m
-${matlab_exec} -nojvm -nodisplay -nosplash < drugCIPHER_part_2_exec.m
-rm drugCIPHER_part_2_exec.m
